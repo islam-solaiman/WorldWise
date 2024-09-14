e311cf4db635485ef5bb07d630e87ff64e539f51
islam solaiman <islam.solaiman92@gmail.com>
2024-09-14 23:20:46 +0400

    Adde the App Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/appLayout";
+import Login from "./pages/Login";
+import "./index.css"
+
 function App() {
 
 
   return (
-    <div>
-      WorldWise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Homepage/>}/>
+        <Route  path="product" element={<Product/>} />
+        <Route path ="pricing" element={<Pricing/>}/>
+        <Route path ="app" element={<AppLayout/>}/>
+        <Route path ="*" element={<PageNotFound/>}/>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter> 
   )
 }
 
