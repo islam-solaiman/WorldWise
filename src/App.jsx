ed722e2ca5d2c0ce4f01dc77dc24d5486433b728
islam solaiman <islam.solaiman92@gmail.com>
2024-09-21 01:15:06 +0400

    Impelementation of cities list and countries list

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,51 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/appLayout";
 import Login from "./pages/Login";
 import "./index.css"
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+
+
+
+const BASE_URL = 'http://localhost:8000';
 
 function App() {
+  const [cities, setCities] = useState([]); 
+  const [isLoading, SetIsLoading] = useState(false);
+
+  useEffect(function() {
+    async function fetchCities() {
+      try {
+        SetIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data ....");
+      } finally {
+        SetIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, [])
 
 
   return (
+
     <BrowserRouter>
       <Routes>
         <Route path ="/" element={<Homepage/>}/>
         <Route  path="product" element={<Product/>} />
         <Route path ="pricing" element={<Pricing/>}/>
-        <Route path ="app" element={<AppLayout/>}>
-          <Route index element={<p>List Of Cities</p>}/>
-          < Route path="cities" element={<p>List Of Cities</p>}/>
-          < Route path="countries" element={<p>Countries</p>}/>
+        <Route path ="app" element={<AppLayout />}>
+          <Route
+            index 
+            element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          < Route 
+            path="cities" 
+            element={<CityList cities={cities} isLoading={isLoading}/>}/>
+          < Route path="countries" element={<CountryList 
+          cities={cities} isLoading={isLoading}/>}/>
           < Route path="form" element={<p>Form</p>}/>
         </Route>
         <Route path ="*" element={<PageNotFound/>}/>
