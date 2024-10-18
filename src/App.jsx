c22e3a9793fe6166c542732484518efe431fcf3a
islam solaiman <islam.solaiman92@gmail.com>
2024-10-19 01:55:19 +0400

    Added programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -10,6 +10,7 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import { useEffect, useState } from "react";
 import City from "./components/City";
+import Form  from "./components/Form";
 
 
 const BASE_URL = 'http://localhost:8000';
@@ -45,14 +46,14 @@ function App() {
         <Route path ="app" element={<AppLayout />}>
           <Route
             index 
-            element={<CityList cities={cities} isLoading={isLoading}/>}/>
+            element={<Navigate to='cities' replace/>}/>
           < Route 
             path="cities" 
             element={<CityList cities={cities} isLoading={isLoading}/>}/>
           <Route path="cities/:id" element={<City />}/>
           < Route path="countries" element={<CountryList 
           cities={cities} isLoading={isLoading}/>}/>
-          < Route path="form" element={<p>Form</p>}/>
+          < Route path="form" element={<Form />}/>
         </Route>
         <Route path ="*" element={<PageNotFound/>}/>
         <Route path="login" element={<Login />} />
